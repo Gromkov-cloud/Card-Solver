8b5e8cb7c96be191a83537a61dd93f87acb516a1
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-10-11 14:36:40 +0500

    add gulp-file-include plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ const { src, dest, parallel, series, watch } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean')
+const fileinclude = require("gulp-file-include")
 
 const paths = {
     html: {
-        src: "./src/**/*.html",
+        src: "./src/index.html",
         dest: "./app/"
     },
     styles: {
@@ -24,48 +25,63 @@ const paths = {
         src: "./src/images/**",
         dest: "./app/images/"
     },
+    htmlInclude: {
+        src: "./src/Components/**",
+        dest: "./app/"
+    },
     clean: {
         src: "./app/"
     }
 }
 
-function html () {
+function html() {
     return src(paths.html.src)
-    .pipe(dest(paths.html.dest))
+        .pipe(dest(paths.html.dest))
 }
 
-function styles () {
+function styles() {
     return src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest(paths.styles.dest))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(paths.styles.dest))
 }
 
-function scripts () {
+function scripts() {
     return src(paths.scripts.src)
-    .pipe(dest([paths.scripts.dest]))
+        .pipe(dest([paths.scripts.dest]))
 }
 
-function images () {
+function images() {
     return src(paths.images.src)
-    .pipe(dest(paths.images.dest))
+        .pipe(dest(paths.images.dest))
 }
 
-function fonts () {
+function fonts() {
     return src(paths.fonts.src)
-    .pipe(dest(paths.fonts.dest))
+        .pipe(dest(paths.fonts.dest))
 }
 
-function appClean () {
+function appClean() {
     return src(paths.clean.src)
-    .pipe(clean())
+        .pipe(clean())
 }
 
-function gulpWatch () {
-    watch(paths.html.src, html);
+function include() {
+    return src(paths.html.src)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest(paths.html.dest));
+}
+
+function gulpWatch() {
+    watch(paths.html.src, include);
+    watch(paths.htmlInclude.src, include)
     watch(paths.styles.src, styles);
     watch(paths.scripts.src, scripts)
     watch(paths.fonts.src, fonts);
     watch(paths.images.src, images)
 }
 
-exports.default = series( appClean, parallel(html, styles, scripts, fonts, images), gulpWatch );
+
+exports.default = series(appClean, parallel(include, styles, scripts, fonts, images), gulpWatch);
