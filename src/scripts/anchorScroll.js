630f2c2ec9c3db396e2322097f820c3fdff5420f
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-11-10 23:31:49 +0500

    close mobile menu at anchor click

--- src/scripts/anchorScroll.js
+++ src/scripts/anchorScroll.js
@@ -4,6 +4,16 @@ for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
         e.preventDefault()
 
+        if (burgerBtn.classList.contains("burger_active")) {
+            burgerBtn.classList.remove("burger_active")
+            bodyOverlay.classList.remove("body-overlay_opacity")
+            menuMobile.classList.remove("menu-mobile_active")
+            setTimeout(() => {
+                bodyOverlay.classList.remove("body-overlay_active")
+                body.classList.remove("body_hidden")
+            }, 260)
+        }
+
         const blockID = anchor.getAttribute('href')
 
         document.getElementById(blockID).scrollIntoView({
