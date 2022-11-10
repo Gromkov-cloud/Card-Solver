d128223c8711447b03be7c84143d640b79d670fe
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-11-11 00:29:53 +0500

    split language selectors functionality

--- src/scripts/languageSelector.js
+++ src/scripts/languageSelector.js
@@ -1,20 +1,31 @@
-const langSwitcher = document.querySelector(".language-selector")
-const langSwitchList = document.querySelector(".language-selector__list")
-const langSwitchLangs = document.querySelectorAll(".language-selector__language")
-const langSwitchCurrentLang = document.querySelector(".language-selector__current-language")
-const langSwitchArrow = document.querySelector(".language-selector__arrow")
+const allLangSelectors = document.querySelectorAll(".language-selector")
 
-langSwitcher.addEventListener("click", (e) => {
-    langSwitchList.classList.toggle("language-selector__list_active")
-    langSwitchArrow.classList.toggle("language-selector__arrow_open")
-    if (e.target.classList.contains("language-selector__language")) {
-        langSwitchLangs.forEach(lang => {
-            lang.classList.remove("language-selector__language_current")
-        })
-        e.target.classList.add("language-selector__language_current")
-        langSwitchList.classList.remove("language-selector__list_active")
-        langSwitchArrow.classList.remove("language-selector__arrow_open")
-        langSwitchCurrentLang.textContent = e.target.textContent
-        console.log(`Lang is switched to ${e.target.textContent}`)
-    }
-})
+const langSelector = (id) => {
+    const langSwitcher = document.getElementById(id)
+    const langSwitchList = langSwitcher.querySelector(".language-selector__list")
+    const langSwitchLangs = langSwitcher.querySelectorAll(".language-selector__language")
+    const langSwitchCurrentLang = langSwitcher.querySelector(".language-selector__current-language")
+    const langSwitchArrow = langSwitcher.querySelector(".language-selector__arrow")
+
+    console.log(allLangSelectors)
+
+    langSwitcher.addEventListener("click", (e) => {
+        langSwitchList.classList.toggle("language-selector__list_active")
+        langSwitchArrow.classList.toggle("language-selector__arrow_open")
+        if (e.target.classList.contains("language-selector__language")) {
+
+
+            langSwitchLangs.forEach(lang => {
+                lang.classList.remove("language-selector__language_current")
+            })
+            e.target.classList.add("language-selector__language_current")
+            langSwitchCurrentLang.textContent = e.target.textContent
+
+            langSwitchList.classList.remove("language-selector__list_active")
+            langSwitchArrow.classList.remove("language-selector__arrow_open")
+        }
+    })
+}
+
+langSelector("lang-selector-1")
+langSelector("lang-selector-2")
